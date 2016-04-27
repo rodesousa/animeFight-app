c93b4e6f25ab3245ac4f7cd5882cceb6bc999f6f
rodesousa <dessroberto@gmail.com>
2016-04-27 17:03:07 +0200

    add item in inventory

--- animeFight/src/main/java/fr/android/animefight/Activity/home/inventory/InventoryActivity.java
+++ animeFight/src/main/java/fr/android/animefight/Activity/home/inventory/InventoryActivity.java
@@ -2,7 +2,10 @@ package fr.android.animefight.Activity.home.inventory;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.ScrollView;
 import fr.android.animefight.R;
 import fr.android.animefight.bean.inventory.CaseInventory;
@@ -24,15 +27,32 @@ public class InventoryActivity extends Activity {
         controllerInventory = new ControllerInventory((Model) getIntent().getSerializableExtra("Model"));
         ScrollView view = (ScrollView) findViewById(R.id.scrollView);
 
-
         for (CaseInventory caseInventory : controllerInventory.getModel().getPlayer().getInventory().getCases()) {
-            ImageView imageView = new ImageView(this);
+            ImageButton imageButton = new ImageButton(this);
             ScrollView.LayoutParams layoutParams = new ScrollView.LayoutParams(ScrollView.LayoutParams.WRAP_CONTENT,
                     ScrollView.LayoutParams.WRAP_CONTENT);
-            imageView.setLayoutParams(layoutParams);
-            imageView.setBackgroundResource(caseInventory.getItem().getImagePath());
-            view.addView(imageView);
+            imageButton.setLayoutParams(layoutParams);
+            imageButton.setBackgroundResource(caseInventory.getItem().getImagePath());
+            view.addView(imageButton);
+            imageButton.setOnClickListener(new View.OnClickListener() {
+                public void onClick(final View v) {
+                    descript_item(v);
+                }
+            });
         }
     }
+
+    private void descript_item(View view) {
+        ImageView viewById = (ImageView) findViewById(R.id.descri_item);
+        viewById.setBackgroundResource(R.drawable.naru);
+        LinearLayout linearLayout = (LinearLayout) findViewById(R.id.linearInventory);
+        linearLayout.setVisibility(0);
+    }
+
+    public void backInventory(View view){
+        LinearLayout linearLayout = (LinearLayout) findViewById(R.id.linearInventory);
+        linearLayout.setVisibility(4);
+    }
+
 
 }
