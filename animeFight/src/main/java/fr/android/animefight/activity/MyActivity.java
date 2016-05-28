4ebce87f351c40889693affaa2940c7f5b636ec5
rodesousa <dessroberto@gmail.com>
2016-05-28 12:10:53 +0200

    add login call ws

--- animeFight/src/main/java/fr/android/animefight/activity/MyActivity.java
+++ animeFight/src/main/java/fr/android/animefight/activity/MyActivity.java
@@ -3,9 +3,13 @@ package fr.android.animefight.activity;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 import fr.android.animefight.R;
 import fr.android.animefight.ws.AsyncResponse;
 import fr.android.animefight.ws.WsClient;
+import org.json.JSONException;
+
+import java.io.UnsupportedEncodingException;
 
 /**
  * La toute premièere activity. C'est celle ou on se log
@@ -20,19 +24,23 @@ public class MyActivity extends Activity implements AsyncResponse{
         setContentView(R.layout.main);
     }
 
-    public void callCoreAct(View view) {
-        invokeWS(view);
+    public void callCoreAct(View view) throws UnsupportedEncodingException, JSONException {
+        invokeWS();
 //        Intent intent = new Intent(this, CoreActivity.class);
 //        startActivity(intent);
     }
 
-    public void invokeWS(View view) {
-        WsClient.get("https://sleepy-everglades-20389.herokuapp.com/db", this);
+    public void invokeWS() throws UnsupportedEncodingException, JSONException {
+        WsClient.login("https://sleepy-everglades-20389.herokuapp.com/signin", this, this.getApplicationContext());
     }
 
     @Override
     public void processFinish(boolean state) {
-        System.out.println(state);
 
+        if (state){
+            Toast.makeText(getApplicationContext(), "true", Toast.LENGTH_LONG).show();
+        }else{
+            Toast.makeText(getApplicationContext(), "false", Toast.LENGTH_LONG).show();
+        }
     }
 }
--- animeFight/src/main/java/fr/android/animefight/ws/WsClient.java
+++ animeFight/src/main/java/fr/android/animefight/ws/WsClient.java
@@ -1,18 +1,49 @@
 package fr.android.animefight.ws;
 
+import android.content.Context;
 import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.RequestParams;
+import cz.msebera.android.httpclient.entity.StringEntity;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.UnsupportedEncodingException;
 
 /**
  * Created by rodesousa on 25/05/16.
  */
 public class WsClient {
 
-    public static void get(String url, AsyncResponse asyncResponse) {
-        AsyncHttpClient client = new AsyncHttpClient();
-        client.setMaxConnections(5);
+    public static void get(String url, AsyncResponse asyncResponse, Context applicationContext) {
+        //curl --header "Content-type: application/json" --request POST --data '{"name": "Toto", "password": "aa"}' https://sleepy-everglades-20389.herokuapp.com/signin
+        AsyncHttpClient client = init();
 
         HttpResponse responseHandler = new HttpResponse(asyncResponse);
         client.get(url, responseHandler);
+    }
+
+
+    public static void login(String url, AsyncResponse asyncResponse, Context applicationContext) throws JSONException, UnsupportedEncodingException {
+        AsyncHttpClient client = init();
+        RequestParams requestParams = new RequestParams();
+
+        HttpResponse responseHandler = new HttpResponse(asyncResponse);
+
+        JSONObject jsonParams = new JSONObject();
+        jsonParams.put("name", "Toto");
+        jsonParams.put("password", "aa");
+        StringEntity entity = new StringEntity(jsonParams.toString());
+        client.post(applicationContext,url, entity, "application/json", responseHandler);
+
+//        client.addHeader(AsyncHttpClient.HEADER_CONTENT_TYPE, "application/json");
+
+//        client.post(url, requestParams, responseHandler);
+    }
+
+    public static AsyncHttpClient init() {
+        AsyncHttpClient client = new AsyncHttpClient();
+        client.setMaxConnections(5);
+        return client;
     }
 
 }
